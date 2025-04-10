0eb3f76c2b330228e547f68f8d3aca22f8475d8b
Mathias Nielsen <mgni62378@edu.ucl.dk>
2025-04-10 10:42:19 +0200

    Made Calculator and tested Calculator

---
+++ Lommeregner/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Lommeregner
+{
+    public interface ICalculatoro
+    {
+        int Add(int a, int b);
+        int Subtract(int a, int b);
+        int Multiply(int a, int b);
+        double Divide(int a, int b);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+
+}
